38d88069a93459a74ce56fe8606deb5002a85a98
Alex <alexyoung1992@gmail.com>
2014-08-15 17:23:47 -0700

    Renamed resume file

--- src/resume.php
+++ src/resume.php
@@ -157,7 +157,7 @@ with front-end GUI objects</li>
         $mustache = new Mustache_Engine(array(
             'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
         ));
-        $button = new Button_Blue("Alexander Young - Resume.pdf", "Download A Copy");
+        $button = new Button_Blue("Alexander_Young_Resume.pdf", "Download A Copy");
         echo $mustache->render('button', $button);
     ?>
 </div>
